2a72838fc6e6548e224142d0462e6579ee7fdfbd
Minh Nguyen <minhnewin@gmail.com>
2023-05-26 13:44:37 -0700

    V13.1
    Changed elements and finalized for class project submission

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -47,7 +47,7 @@ typewriter.typeString('Hey there!')
     .start();
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
+window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
@@ -100,4 +100,24 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
     console.log(current);
   });
+}
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener("scroll", scrolll);
+
+function scrolll() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
